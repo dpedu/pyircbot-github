4122667a67901006b71860c5b4dc9d1c9177f00b
Dave Pedu <dave@Daves-MacBook-Air.local>
2012-10-15 17:00:27 -0400

    Updated messageHasCommand method in Tools

--- botlibs/Tools.py
+++ botlibs/Tools.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
-def messageHasCommand(command, message):
-	if message[0:len(command)+1] == command+" " or (message[0:len(command)] == command and len(command) == len(command)):
-		return (True, command, message[len(command)+1:], message)
-	else:
-		return False;
+def messageHasCommand(command, message, requireArgs=False):
+	# Check if the message at least starts with the command
+	messageBeginning = message[0:len(command)]
+	if messageBeginning!=command:
+		return False
+	# Make sure it's not a subset of a longer command (ie .meme being set off by .memes)
+	subsetCheck = message[len(command):len(command)+1]
+	if subsetCheck!=" " and subsetCheck!="":
+		return False
+	
+	# We've got the command! Do we need args?
+	argsStart = len(command)
+	args = ""
+	if argsStart > 0:
+		args = message[argsStart+1:]
+	
+	if requireArgs and args.strip() == '':
+		return False
+	
+	# Verified! Return the set.
+	return (True, command, args, message)
